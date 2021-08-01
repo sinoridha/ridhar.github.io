f8eab4ed7181c91da3796058931891285876d381
Ridha Ramadhansyah <ridha.ramadhansyah@sehatq.com>
2021-08-01 17:08:26 +0700

    Rebuilding site Sun Aug  1 17:08:26 WIB 2021

--- deploy.sh
+++ deploy.sh
@@ -5,15 +5,20 @@ echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 # Build the project.
 hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
 
-# Add changes to git.
+## Redirect to medium ridhar.medium.com
+sed -i '' '10i\
+  <meta http-equiv="refresh" content="0; url=https://ridhar.medium.com/database-quran-indonesia-untuk-pengembangan-aplikasi-5a27cf3f643b"/>
+' docs/database-quran-indonesia/index.html
+
+# # Add changes to git.
 git add .
 
-# Commit changes.
+# # Commit changes.
 msg="Rebuilding site `date`"
 if [ $# -eq 1 ]
   then msg="$1"
 fi
 git commit -m "$msg"
 
-# Push source and build repos.
+# # Push source and build repos.
 git push origin master
--- readme.md
+++ readme.md
@@ -12,5 +12,3 @@ Create new content ```hugo new "tell-dont-ask.md"```
 
 Location of side bar ```themes/hyde/layouts/partials/sidebar.html```
 Post layout ```themes/hyde/layouts/_default/single.html```
-
-
